242b0d60fb675ab022ca950399188b579a36afe2
Владимир <vovanpadawan@gmail.com>
2018-04-16 22:51:07 +0300

    Домашка по 7му уроку, сразу с исправлениями из комментариев преподавателя

---
+++ Lesson7Homework/Employees/Employees/EmployeeList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    class EmployeeList : ViewModel
+    {
+        //public event PropertyChangedEventHandler PropertyChanged;
+
+        public readonly ObservableCollection<Employee> List;
+
+        //конструктор без параметров
+        public EmployeeList()
+        {
+            //заполняю список сотрудниками из базы данных в конструкторе класса
+            List = new ObservableCollection<Employee>(DBconnector.GetPeople());
+        }
+        /// <summary>
+        /// Публичный метод для добавления сотрудника в коллекцию
+        /// </summary>
+        /// <param name="employee">Тип сотрудник</param>
+        public void AddEmployee(Employee employee)
+        {
+            List.Add(employee);
+            //Заготовка для команды добавляения сотрудника
+            DBconnector.AddEmployee(employee.Name);
+            OnPropertyChanged(nameof(List));
+        }
+
+        /// <summary>
+        /// Публичный метод удаления сотрудника
+        /// </summary>
+        /// <param name="employee">Тип сотрудник, которого надо удалить</param>
+        public void Remove(Employee employee)
+        {
+            if (List.Contains(employee))
+                List.Remove(employee);
+            OnPropertyChanged(nameof(List));
+        }
+    }
+}
---
+++ Lesson7Homework/Employees/Employees/Service/ViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using Employees.Annotations;
+
+namespace Employees
+{
+	/// <summary>Модель-представления</summary>
+	abstract class ViewModel :
+		MarkupExtension, 		 // Говорим, что модель является расширением разметки XAML
+		INotifyPropertyChanged	 // Поддерживает уведомления изменений свйоств
+	{
+		/// <summary>Метод, вызвыаемый процессором XAML при создании логического дерева интерфейса</summary>
+		public override object ProvideValue(IServiceProvider sp) => this;
+
+		/// <summary>Событие, возникающие когда объект изменяет значение своего свойства</summary>
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		/// <summary>Метод, генерирующий событие <see cref="PropertyChanged"/></summary>
+		/// <param name="PropertyName">Имя изменившегося свойства (оставить пустым что бы система сама его определила)</param>
+		[NotifyPropertyChangedInvocator]
+		protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+
+		/// <summary>Метод, упрощающий установку значений полей свойств в сеттерах</summary>
+		/// <typeparam name="T">Тип значения поля</typeparam>
+		/// <param name="field">Поле, хранящее значение свйоства</param>
+		/// <param name="value">Устанавливаемое значение</param>
+		/// <param name="PropertyName">Имя изменяемого свойства</param>
+		/// <returns>Истина, если значение поля было изменено</returns>
+		[NotifyPropertyChangedInvocator]
+		protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+		{
+			if(Equals(field, value)) return false;
+			field = value;
+			OnPropertyChanged(PropertyName);
+			return true;
+		}
+	}
+}
